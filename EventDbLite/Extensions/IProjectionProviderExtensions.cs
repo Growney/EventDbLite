c0faa0b48dfb8527683687f783f3f9a89243f69e
Growney <rowney.graeme@googlemail.com>
2026-05-04 18:28:23 +0100

    projection extension fixes

---
+++ EventDbLite/Extensions/IProjectionProviderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using EventDbLite.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventDbLite.Abstractions;
+
+public static class IProjectionProviderExtensions
+{
+    public static Task<T> Load<T>(this IProjectionProvider provider) => provider.Load<T>(null, StreamPosition.End);
+    public static Task<T> Load<T>(this IProjectionProvider provider, string streamName) => provider.Load<T>(streamName, StreamPosition.End);
+    public static Task<T> Load<T>(this IProjectionProvider provider, StreamPosition until) => provider.Load<T>(null, until);
+}
